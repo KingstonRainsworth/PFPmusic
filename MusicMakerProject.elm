216f0c6885d7b5d00b2649b51c17eb49b6bd1c93
RyanShuman <rysh4155@colorado.edu>
2018-04-15 16:19:32 -0600

    Add files via upload
    updated root stuff

--- MusicMakerProject.elm
+++ MusicMakerProject.elm
@@ -19,6 +19,8 @@ type alias ProbInChord = {ic : Float} -- probability chord includes notes from m
 type alias ProbRestMelody = {prm : Float}
 type alias ProbRestChord = {prc : Float}
 
+type alias StartAtRoot = {sar : Bool}
+type alias EndAtRoot = {ear : Bool}
 
 --------patternization inputs ------------------------------------------------------
 {-type alias ProbPatternMatch = {ppm : Float} -- probability that the next note is a pattern
@@ -138,13 +140,13 @@ type Dur = W|H|Q|EI|S
 
 type alias Note = Int
 
-type alias ChordR = List Dur
+type alias ChordR = List Int
 
 type alias Melody = List Int
 
-type alias Chord = List (Note,Note,Note)
+type alias Chord = List (List Int)
 
-type alias MelodyR = List Dur
+type alias MelodyR = List Int
 
 notes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88]
 
@@ -211,16 +213,32 @@ let ppa = patternCreator --stuff here
 
 
 
+-------------------------------start/ end at root section --------------------------------------
 
+startAtRoot : KeySignatureSuggestion -> StartAtRoot -> Melody -> Melody
+startAtRoot kss sar m =
+  if sar == True then let n = kss + 48 in
+    List.drop 1 m
+    List.append [n] m
+    m
+    else m
 
-
+endAtRoot : KeySignatureSuggestion -> EndAtRoot -> Melody -> Melody
+endAtRoot kss sar m =
+  if sar == True then let n = kss + 48 in
+    List.reverse m
+    List.drop 1 m
+    List.append [n] m
+    List.reverse m
+    m
+    else m
 
 
 ------------------------------ start program flow section --------------------------------------
-
+{-
 workFlowMain : -> (MelodyR, ChordR, Melody, Chord)
 workFlowMain   =
-
+-}
 workFlowMainLoop : PatternList -> Int -> Int-> MelodyR -> ChordR -> Melody -> Chord -> (MelodyR, ChordR, Melody, Chord)
 workFlowMainLoop pl siz maxsiz mr cr m c  =
   if (maxsiz - siz) < 4 then let tmr = melodyRhythm 0 siz se mr pmr in
